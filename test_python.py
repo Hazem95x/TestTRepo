d5bbc2958af30af3bc309788e3100b87ccabe1f1
Hazem Omar <Hazematwwa@gmail.com>
2023-01-29 14:05:51 +0200

    fourth modified in python

--- test_python.py
+++ test_python.py
@@ -1,5 +1,5 @@
 import os ; 
-import numpy ;
+#import numpy ;
 import sys ;
 
 
@@ -7,6 +7,37 @@ print(os.path)
 
 print("Hazem Omar")
 
-print (sys.path_hooks)
+print (sys.path_hooks);
+
+
+## make some of mathamatical operation 
+first_ope = 5
+second_ope = 9 
+
+print (first_ope // second_ope)
+
+
+
+print("Hello '\n' Python")
+
+print('Hello , Python')
+
+print('\t Hazem')
+      
+
+type(first_ope)
+
+name_ =  input("please enter your name")
+Age  = int(input("please enter your age "))
+
+Birth  = Age- 2023 ;  
+
+print("your name is" + " " + name_ + "your age is " + "" + Birth)
+
+
+
+
+
+
 
 
